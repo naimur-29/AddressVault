ebfd3b176561b4677071024974a69124394f9a89
Naim <skylike2904@gmail.com>
2023-06-22 22:47:15 +0600

    modified:   src/components/dashboard/ContactItem.tsx         new file:   src/components/dashboard/ContactMenu.tsx         modified:   src/components/sidebar/Sidebar.tsx         modified:   src/global.css

--- src/components/dashboard/ContactItem.tsx
+++ src/components/dashboard/ContactItem.tsx
@@ -1,12 +1,15 @@
+// components:
+import ContactMenu from "./ContactMenu";
+
 // types:
 import { ContactItemType } from "../../@types/contact";
 
 // main:
 const ContactItem = ({ imgLink, name, contact }: ContactItemType) => {
   return (
-    <div className="flex items-center justify-between bg-[--primary-violet-op22] rounded p-2 cursor-pointer hover:bg-[--primary-violet-op33] duration-200 shadow-lg hover:shadow-none">
-      <div className="_left flex items-center gap-2">
-        <div className="_img-container w-12 h-12 overflow-hidden rounded-[50%] bg-[--primary-violet-op33]">
+    <div className="flex items-center justify-between rounded p-2 cursor-pointer bg-[--primary-violet-op22] hover:bg-[--primary-violet-op33] duration-200 shadow-lg hover:shadow-none border-b border-[--primary-violet-op55]">
+      <div className="flex items-center gap-2 _left">
+        <div className="_img-container w-12 h-12 overflow-hidden rounded-[50%] bg-[--primary-violet-op55]">
           {imgLink ? (
             <img
               src={imgLink}
@@ -14,7 +17,7 @@ const ContactItem = ({ imgLink, name, contact }: ContactItemType) => {
               className="object-cover rounded-full"
             />
           ) : (
-            <div className="uppercase rounded-full flex items-center justify-center w-full h-full text-[--secondary-text-dim] font-semibold">
+            <div className="uppercase rounded-full flex items-center justify-center w-full h-full text-[--primary-violet] font-semibold">
               {name.split(" ")[0][0] + name.split(" ")[1][0]}
             </div>
           )}
@@ -25,11 +28,7 @@ const ContactItem = ({ imgLink, name, contact }: ContactItemType) => {
         </div>
       </div>
 
-      <div className="_right flex items-center justify-center gap-1 h-full p-2 aspect-square rounded-full cursor-pointer active:scale-110">
-        <div className="_dot w-1 h-1 rounded-full bg-[--primary-text-slate]"></div>
-        <div className="_dot w-1 h-1 rounded-full bg-[--primary-text-slate]"></div>
-        <div className="_dot w-1 h-1 rounded-full bg-[--primary-text-slate]"></div>
-      </div>
+      <ContactMenu />
     </div>
   );
 };
---
+++ src/components/dashboard/ContactMenu.tsx
@@ -0,0 +1,57 @@
+// importing libraries:
+import { useState } from "react";
+import { motion, AnimatePresence } from "framer-motion";
+
+// animations:
+import { FadeInIn } from "../../animations/sidebar";
+
+const ContactMenu = () => {
+  const [isContactMenuActive, setIsContactMenuActive] =
+    useState<boolean>(false);
+
+  return (
+    <>
+      <div
+        onClick={() => setIsContactMenuActive(true)}
+        className="flex items-center justify-center h-full gap-1 p-2 rounded-full cursor-pointer _right aspect-square active:scale-110"
+      >
+        <div className="_dot w-1 h-1 rounded-full bg-[--primary-violet]"></div>
+        <div className="_dot w-1 h-1 rounded-full bg-[--primary-violet]"></div>
+        <div className="_dot w-1 h-1 rounded-full bg-[--primary-violet]"></div>
+      </div>
+
+      <AnimatePresence>
+        {isContactMenuActive && (
+          <motion.div
+            variants={FadeInIn}
+            initial="hidden"
+            animate="visible"
+            exit="exit"
+            className="_wrapper fixed top-0 right-0 w-full sm:w-[calc(100vw-270px)] h-full bg-[--primary-violet-op33] sm:backdrop-blur-sm flex items-center justify-center px-5 z-50"
+          >
+            <motion.div
+              variants={FadeInIn}
+              className="_modal w-full md:w-[540px] min-h-[50%] bg-white rounded text-black"
+            >
+              <button onClick={() => setIsContactMenuActive(false)}>
+                Close
+              </button>
+
+              <div>
+                <label htmlFor="">Username</label>
+                <input type="text" name="" id="" />
+              </div>
+
+              <div>
+                <label htmlFor="">PhotoUrl</label>
+                <input type="text" name="" id="" />
+              </div>
+            </motion.div>
+          </motion.div>
+        )}
+      </AnimatePresence>
+    </>
+  );
+};
+
+export default ContactMenu;
